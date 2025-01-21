7600bcaa296a45dfe1b58ba75a325a99b4d723f8
bhokumar <bhoopendra.akgec@gmail.com>
2025-01-22 01:30:53 +0530

    Adding initial project

---
+++ src/main/resources/db/migration/V1_0_0__CreateTables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS `Author` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `birthdate` DATE
+);
+
+CREATE TABLE IF NOT EXISTS `Book` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(100) NOT NULL,
+    `author_id` INT NOT NULL,
+    `published_date` DATE,
+    `isbn` VARCHAR(20) UNIQUE,
+    `genre` VARCHAR(50),
+    `available` BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (`author_id`) REFERENCES `Author`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `Member` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) UNIQUE,
+    `membership_date` DATE
+);
+
+CREATE TABLE IF NOT EXISTS `Loan` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `book_id` INT NOT NULL,
+    `member_id` INT NOT NULL,
+    `loan_date` DATE NOT NULL,
+    `return_date` DATE,
+    FOREIGN KEY (`book_id`) REFERENCES `Book`(`id`),
+    FOREIGN KEY (`member_id`) REFERENCES `Member`(`id`)
+);
---
+++ src/test/java/org/fusionovate/library/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fusionovate.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
