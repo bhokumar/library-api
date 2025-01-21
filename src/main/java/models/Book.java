cd42d7ec2094fdab143a3b06fa0abaadd1ed9c5b
bhokumar <bhoopendra.akgec@gmail.com>
2025-01-22 01:42:11 +0530

    Adding some initial models

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,82 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Book {
+    private int id;
+    private String title;
+    private String isbn;
+    private int authorId;
+    private LocalDate publishedDate;
+    private String genre;
+    private boolean isAvailable;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String isbn, int authorId, LocalDate publishedDate, String genre, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.authorId = authorId;
+        this.publishedDate = publishedDate;
+        this.genre = genre;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
---
+++ src/main/java/models/Loan.java
@@ -0,0 +1,70 @@
+package models;
+
+public class Loan {
+    private int id;
+    private int bookId;
+    private int memberId;
+    private String issueDate;
+    private String dueDate;
+    private String returnDate;
+
+    public Loan() {
+    }
+
+    public Loan(int id, int bookId, int memberId, String issueDate, String dueDate, String returnDate) {
+        this.id = id;
+        this.bookId = bookId;
+        this.memberId = memberId;
+        this.issueDate = issueDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+}
